af2055915b637661493a6a3daf4a7e8a409d0bc5
liutong <lt_felicity@163.com>
2017-04-03 20:33:53 +0800

    create project

---
+++ gitOrders/gitOrders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitOrders
+//
+//  Created by liutong on 2017/4/3.
+//  Copyright © 2017年 liutong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
